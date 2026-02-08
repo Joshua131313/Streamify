f81ef849cb91c6d0da3e6e6ddf49c528671a0120
Joshua Bitton <joshtosh13@hotmail.com>
2026-02-07 22:10:09 -0500

    Restyled navigation arrows on swiper

--- src/components/media/MediaRail/MediaRailSwiper.tsx
+++ src/components/media/MediaRail/MediaRailSwiper.tsx
@@ -9,6 +9,8 @@ import { MediaCard } from '../../ui/MediaCard/MediaCard';
 import { Top10MediaCard } from '../../ui/MediaCard/Top10MediaCard';
 import { useMediaDiscover } from '../../../hooks/mediaHooks/useMediaDiscover';
 import { MediaSkeletonCard } from '../../ui/MediaCard/SkeletonCards/MediaSkeletonCard';
+import { useRef } from 'react';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 export const MediaRailSwiper = () => {
     const { genre, provider, mediaType, category, variant } = useMediaRail();
@@ -18,6 +20,11 @@ export const MediaRailSwiper = () => {
         genreId: genre,
         provider,
     })
+
+    const prevRef = useRef<HTMLDivElement | null>(null);
+    const nextRef = useRef<HTMLDivElement | null>(null);
+
+
     const swiperSlides = media.map((m, i) => {
         return (
             <SwiperSlide key={m.id}>
@@ -40,20 +47,33 @@ export const MediaRailSwiper = () => {
     })
 
     return  (
-        <Swiper
-            className={`media-swiper ${variant === "top10" && "top-10-media-swiper"}`}
-            slidesPerView={"auto"}
-            spaceBetween={variant === "top10" ? 100 : 10}
-            navigation
-            modules={[Navigation]}
-        >
-            {
+        <div className="swiper-container">
+            <div ref={prevRef} className="prev-button swiper-nav-button">
+                <FaChevronLeft />
+            </div>
+            <div ref={nextRef} className="next-button swiper-nav-button">
+                <FaChevronRight />
+            </div>
+            <Swiper
+                className={`media-swiper ${variant === "top10" && "top-10-media-swiper"}`}
+                slidesPerView={"auto"}
+                spaceBetween={variant === "top10" ? 100 : 10}
+                navigation
+                modules={[Navigation]}
+                onBeforeInit={(swiper) => {
+                    //@ts-ignore
+                    swiper.params.navigation.prevEl = prevRef.current;
+                    //@ts-ignore
+                    swiper.params.navigation.nextEl = nextRef.current;
+                }}
+            >
+                {
                 isLoading ?
                     skeletonSlides
                     :
                     swiperSlides
-                    
-            }
-        </Swiper>
+                }
+            </Swiper>
+        </div>
     )
 }
