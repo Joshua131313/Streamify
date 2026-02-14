b12b64d036287f771a7592607f2a07c31f4d5ae4
Joshua Bitton <joshtosh13@hotmail.com>
2026-02-13 21:54:33 -0500

    Update useHeroMedia.ts

--- src/hooks/mediaHooks/useHeroMedia.ts
+++ src/hooks/mediaHooks/useHeroMedia.ts
@@ -5,7 +5,7 @@ import type { TMDBMedia } from "../../types/TMDBMediaType";
 import type { TMDBMediaQueryResult } from "../../types/tmdb";
 
 export const useHeroMedia = (
-  count: number = 20
+  count: number = 10
 ): TMDBMediaQueryResult => {
 
   const mixed = useMixedMedia("/trending/all/day", count);
