e75396fb10e6cef1854a7fdf11973976394649f0
Joshua Bitton <joshtosh13@hotmail.com>
2026-03-26 20:47:07 -0400

    Adjusted multi watch ui

--- src/pages/Sports/MultiWatchContentEmpty.tsx
+++ src/pages/Sports/MultiWatchContentEmpty.tsx
@@ -3,19 +3,21 @@ import { StyledSelect } from "../../components/ui/StyledSelect/StyledSelect";
 import { useMultiWatch } from "../../context/MultiWatchContext";
 import { useSports } from "../../context/SportsContext";
 import { components } from "react-select";
+import { Button } from "../../components/ui/Button/Button";
+import "./Sports.css"
 
 const CheckboxOption = (props: any) => {
-  return (
-    <components.Option {...props}>
-      <input
-        type="checkbox"
-        checked={props.isSelected}
-        readOnly
-        style={{ marginRight: 8 }}
-      />
-      <label>{props.label}</label>
-    </components.Option>
-  );
+    return (
+        <components.Option {...props}>
+            <input
+                type="checkbox"
+                checked={props.isSelected}
+                readOnly
+                style={{ marginRight: 8 }}
+            />
+            <label>{props.label}</label>
+        </components.Option>
+    );
 };
 export const MultiWatchContentEmpty = () => {
     const { allOfTodaysGames } = useSports();
@@ -43,27 +45,26 @@ export const MultiWatchContentEmpty = () => {
         <div className="multi-watch-empty">
             <span>No Multi Watch Games</span>
 
-<StyledSelect
-  options={options}
-  isMulti
-  value={selectedGames}
-  placeholder="Select games..."
+            <StyledSelect
+                options={options}
+                isMulti
+                value={selectedGames}
+                placeholder="Select games..."
 
-  closeMenuOnSelect={false}
-  hideSelectedOptions={false}
+                closeMenuOnSelect={false}
+                hideSelectedOptions={false}
 
-  components={{ Option: CheckboxOption }}
+                components={{ Option: CheckboxOption }}
 
-  onChange={(opts) => setSelectedGames(opts ? [...opts] : [])}
-/>
+                onChange={(opts) => setSelectedGames(opts ? [...opts] : [])}
+            />
 
-            <button
-                className="multi-watch-add-btn"
-                disabled={selectedGames.length === 0}
+            <Button
                 onClick={handleAddAll}
+                disabled={selectedGames.length === 0}
             >
                 Add Selected Games
-            </button>
+            </Button>
         </div>
     );
 };
