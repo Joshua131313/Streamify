9086106a1cb932578748e6e5b2cad4a7b9a5f76e
Joshua Bitton <joshtosh13@hotmail.com>
2026-04-11 14:30:38 -0400

    Update NetworkIndicatorScreen.tsx

--- src/components/ui/NetworkIndicatorScreen/NetworkIndicatorScreen.tsx
+++ src/components/ui/NetworkIndicatorScreen/NetworkIndicatorScreen.tsx
@@ -1,31 +1,27 @@
 import { FaWifi } from "react-icons/fa";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useOnlineStatus } from "../../../hooks/utilHooks/useOnlineStatus";
-import "./NetworkIndicatorScreen.css"
+import "./NetworkIndicatorScreen.css";
 
 export const NetworkIndicatorScreen = () => {
     const isOnline = useOnlineStatus();
 
-    const [wasOffline, setWasOffline] = useState(false);
     const [showOnline, setShowOnline] = useState(false);
+    const prevIsOnline = useRef(isOnline); 
 
     useEffect(() => {
-        if (!isOnline) {
-            setWasOffline(true);
-        }
-
-        if (isOnline && wasOffline) {
-            // just came back online
+        if (isOnline && !prevIsOnline.current) {
             setShowOnline(true);
-            setWasOffline(false);
 
             const timeout = setTimeout(() => {
                 setShowOnline(false);
-            }, 1000); // show for 1 second
+            }, 1000);
 
             return () => clearTimeout(timeout);
         }
-    }, [isOnline, wasOffline]);
+
+        prevIsOnline.current = isOnline;
+    }, [isOnline]);
 
     if (!isOnline) {
         return (
